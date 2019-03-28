6df9f162d9760302e1c5baacc751b110c92e1e28
turao <lenz.arthur@gmail.com>
2019-03-28 00:19:05 -0300

    remove unecessary white spaces in hermes main

--- src/datapoints/hermes/__main__.py
+++ src/datapoints/hermes/__main__.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     start = time.time()
 
     loop = asyncio.get_event_loop()
-    
+
     # set defaults
     batch_size = 200
     timeout = 5
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         timeout = sys.argv[3]
     except Exception:
         pass
-    
+
     hermes = Hermes(geocoder=OpenCageGeocoder())
     loop.run_until_complete(hermes.enrich(sys.argv[1], batch_size, timeout))
 
